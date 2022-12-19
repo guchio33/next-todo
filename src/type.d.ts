b40cec6f003e5cd3da48983c68a98eb5bdd9db6e
guchio33 <aokihikari3@gmail.com>
2022-12-19 11:12:59 +0900

    データベースの作成

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,22 +1,30 @@
-type Book={
-    id:number;
-    stripe_product_id?: string;
-    stripe_price_id?: string;
-    title: string;
-    description: string;
-    authorId: number;
-    price: number;
-}
+import { Book, Author, User } from '@prisma/client';
+type Book = Book;
 
-type Author = {
-    id: number;
-    name: string;
-};
+type Author = Author;
 
-type User = {
-    uid: string;
-    stripe_customer_id?: string;
-    name: string;
-    email: string;
-    plan: 'free' | 'pro';
-};
+type User = User;
+
+
+// type Book={
+//     id:number;
+//     stripe_product_id?: string;
+//     stripe_price_id?: string;
+//     title: string;
+//     description: string;
+//     authorId: number;
+//     price: number;
+// }
+
+// type Author = {
+//     id: number;
+//     name: string;
+// };
+
+// type User = {
+//     uid: string;
+//     stripe_customer_id?: string;
+//     name: string;
+//     email: string;
+//     plan: 'free' | 'pro';
+// };
